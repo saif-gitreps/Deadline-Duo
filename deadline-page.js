c509d922f675a58ec86b45fe9a8270cddc2eded3
Saif <droconos99@gmail.com>
2023-12-23 22:10:38 +0600

    Added deadline edit page

--- deadline-page.js
+++ deadline-page.js
@@ -1,13 +1,5 @@
-const htmlElement = document.querySelector("html");
-const bodyElement = document.querySelector("body");
-const editAsideElement = document.querySelector("aside");
 const editButtonElement = document.getElementById("edit-btn");
-const deadlineCardElement = document.getElementById("deadline-card");
 
 editButtonElement.addEventListener("click", (event) => {
-   event.preventDefault();
-   deadlineCardElement.preventDefault();
-   htmlElement.classList.toggle("overflow-hidden");
-   bodyElement.classList.toggle("overflow-hidden");
-   editAsideElement.style.display = "block";
+   // This will lead the deadline edit page , will load the edit route.
 });
