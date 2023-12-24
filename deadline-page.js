3e0bd18085a7e828aadea57aa5391712d21c2ef7
Saif <droconos99@gmail.com>
2023-12-24 14:56:48 +0600

    completed task page

--- deadline-page.js
+++ deadline-page.js
@@ -1,6 +1,4 @@
 const editButtonElement = document.getElementById("edit-btn");
-const dropDownButtonElement = document.getElementById("dropdown-btn");
-const dropDownContentElement = document.getElementById("dropdown-content");
 
 editButtonElement.addEventListener("click", (event) => {
    // This will lead the deadline edit page , will load the edit route.
